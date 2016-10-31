bd8f4a0b16ff0c609713a503c27e4d54c11603cc
helloneele <hello@neele.codes>
2016-10-31 16:29:25 +0100

    initial commit, created postcss styleguide

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
+
+gulp.task('build:css', function () {
+    var concat = require('gulp-concat')
+    var postcss = require('gulp-postcss')
+    var autoprefixer = require('autoprefixer')
+    var customProperties = require('postcss-custom-properties')
+    var Import = require('postcss-import')
+    var styleGuide = require('postcss-style-guide')
+    var nano = require('cssnano')
+
+    return gulp.src('./assets/css/app.css')
+        .pipe(postcss([
+            Import,
+            customProperties({ preserve: true }),
+            autoprefixer,
+            styleGuide({
+                project: 'brettlguide.at',
+                dest: 'build/styleguide/index.html',
+                showCode: false,
+                themePath: './assets/styleguide'
+            }),
+            nano
+        ]))
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('./build/styleguide/dist/css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch('./assets/**/*.css', ['build:css']);
+});
--- readme.md
+++ readme.md
@@ -1,1 +1,36 @@
 # brettlguide.at
+## Allgemein
+`npm install` ausführen um alle `node_modules` aus der `package.json` zu installieren.
+
+### Ordnerstruktur
+* __/__
+* __assets__
+  * __css__ (alle rohen CSS Dateien)
+  * __styleguide__ (Template Datei und Template Stylesheet)
+* __build__ (Ordner der hinterher deployed wird)
+  * __styleguide__ (kompilierter Styleguide)
+* __node_modules__
+* __gulpfile.js__ (alle Gulp Tasks)
+* __package.json__ (npm Package-Datei)
+
+### Kompilieren
+Mit `npm install` wird automatisch auch Gulp installiert, siehe `gulpfile.js`.  
+Mit `gulp` startet automatisch der watch Task, der bei einer Änderung in den CSS-Files automatisch kompiliert.
+
+## Styleguide
+In den Stylesheets kann zwischen
+```
+/*
+@styleguide
+
+@title Typography
+
+<<CODE>>
+
+*/
+```
+[Markdown](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) geschrieben werden.  
+
+Um Code-Beispiele zu geben zwei Tabstopps zum Einrücken verwenden.  
+
+__Achtung__ bei den Colors funktioniert das etwas anders, nicht wundern.
