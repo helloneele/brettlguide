73024c0708c986d4b6aa44e8dd8a59670baba6d0
helloneele <hello@neele.codes>
2016-11-28 00:43:55 +0100

    added huetten and map functionality

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,3 +1,7 @@
+import skihuetten from '../../../build/data/skihuetten'
+
+// use map.loaded true/false for preloader image
+
 export default function(long, lat) {
   mapboxgl.accessToken = 'pk.eyJ1IjoiaGVsbG9uZWVsZSIsImEiOiJjaXVlamJoYjEwMDFmMnZxbGk1ZDBzMXdwIn0.i3Sy5G_gVjDLOJ9VcORhcQ'
 
@@ -5,14 +9,43 @@ export default function(long, lat) {
       container: 'map', // container id
       style: 'mapbox://styles/helloneele/ciw0yc5ft00dh2jo5ml0fklvd', //hosted style id
       center: [long, lat], // starting position
-      zoom: 10 // starting zoom
+      zoom: 10, // starting zoom
+      pitch: 45
   });
 
-  setCurrentPos(map, long, lat)
+  //setCurrentPos(map, long, lat)
+  setSkihuetten(map)
+
+
+  map.on('click', function (e) {
+    var features = map.queryRenderedFeatures(e.point, { layers: ['huetten'] });
+
+    if (!features.length) {
+        return;
+    }
+
+    var feature = features[0];
+    // https://www.mapbox.com/mapbox-gl-js/example/center-on-symbol/
+    this.flyTo({center: feature.geometry.coordinates, zoom: 15, pitch: 45});
+    // https://www.mapbox.com/mapbox-gl-js/api/#Popup
+    this.once('moveend', function() {
+      var popup = new mapboxgl.Popup()
+        .setLngLat(feature.geometry.coordinates)
+        .setHTML(feature.properties.name)
+        .addTo(map)
+    });
+
+
+
+
+
+  });
+
 }
 
 
 function setCurrentPos(map, long, lat){
+
   map.on('load', function () {
     map.addSource("points", {
         "type": "geojson",
@@ -21,11 +54,10 @@ function setCurrentPos(map, long, lat){
             "features": [{
                 "type": "Feature",
                 "geometry": {
-                    "type": "Point",
-                    "coordinates": [long, lat]
+                  "type": "Point",
+                  "coordinates": [long, lat]
                 },
                 "properties": {
-                    "title": "Dein Standort",
                     "icon": "marker"
                 }
             }]
@@ -37,15 +69,40 @@ function setCurrentPos(map, long, lat){
         "type": "symbol",
         "source": "points",
         "layout": {
-            "icon-image": "{icon}-15",
-            "text-field": "{title}",
-            "text-font": ["Lato Regular", "Arial Unicode MS Bold"],
-            "text-offset": [0, 0.6],
-            "text-anchor": "top"
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-font": ["Lato Regular", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+
+    });
+  });
+}
+
+function setSkihuetten(map){
+  map.on('load', function () {
+    map.addSource("huetten", {
+        "type": "geojson",
+        "data": skihuetten
+    });
+
+    map.addLayer({
+        "id": "huetten",
+        "type": "symbol",
+        "source": "huetten",
+        "layout": {
+          "icon-image": "rrestaurant-15",
+          "text-field": "{name}",
+          "text-font": ["Lato Regular", "Arial Unicode MS Bold"],
+          "text-size": 12,
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
         },
         "paint": {
-          "icon-color": "#ff0000"
+          "text-color": "#295e72",
+          "icon-color": "#295e72",
         }
     });
-});
+  });
 }
