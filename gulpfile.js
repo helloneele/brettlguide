71f0a6788c7a418f6e8a291abee811ea67412baa
helloneele <hello@neele.codes>
2016-11-27 13:02:25 +0100

    added lost-grid

--- gulpfile.js
+++ gulpfile.js
@@ -17,18 +17,22 @@ gulp.task('copyimg', function() {
 gulp.task('build:css', function () {
     var concat = require('gulp-concat')
     var postcss = require('gulp-postcss')
+    var sourcemaps = require('gulp-sourcemaps')
     var autoprefixer = require('autoprefixer')
+    var lost = require('lost')
     var customProperties = require('postcss-custom-properties')
     var Import = require('postcss-import')
     var styleGuide = require('postcss-style-guide')
     var nano = require('cssnano')
-    var nested = require('postcss-nested');
+    var nested = require('postcss-nested')
 
     return gulp.src('./assets/css/app.css')
+        .pipe(sourcemaps.init())
         .pipe(postcss([
             Import,
             nested,
             customProperties({ preserve: true }),
+            lost(),
             autoprefixer,
             styleGuide({
                 project: 'brettlguide.at',
@@ -36,8 +40,9 @@ gulp.task('build:css', function () {
                 showCode: false,
                 themePath: './assets/styleguide'
             }),
-            nano
+            // nano
         ]))
+        .pipe(sourcemaps.write('./'))
         .pipe(concat('app.min.css'))
         .pipe(gulp.dest('./build/assets/css'))
 })
